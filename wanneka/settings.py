262a11ce67570a705be63cd3086f037b33673188
Francis Ennin <enninfrancis47@gmail.com>
2022-10-27 13:33:36 +0000

    DB settings

--- wanneka/settings.py
+++ wanneka/settings.py
@@ -272,6 +272,6 @@ CORS_ALLOWED_ORIGINS = ["https://*", "http://*"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url 
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
