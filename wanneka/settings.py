69d41999966c1715b5be2580229e8b133330fe59
Francis Ennin <enninfrancis47@gmail.com>
2022-10-27 23:28:57 +0000

    Order and Orderitems redone

--- wanneka/settings.py
+++ wanneka/settings.py
@@ -272,6 +272,7 @@ CORS_ALLOWED_ORIGINS = ["https://*", "http://*"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-# import dj_database_url 
-# prod_db  =  dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+if "DATABASE_URL" in os.environ:
+    import dj_database_url
+
+    DATABASES = {"default": dj_database_url.config()}
